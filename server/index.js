8a9ebaab4bb132eb319135d96a12092baf7680df
Walker <walker@praiseweb.com.br>
2019-02-17 18:09:16 -0300

    fix server tests

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../webpack.config.js';
 
 const app = express();
-app.use(webpackMiddleware(webpack(Object.assign(webpackConfig, {
-    mode: process.env.NODE_ENV || 'development',
-}))));
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.listen(8080, () => {
     console.log('Listening');
--- server/index.spec.js
+++ server/index.spec.js
@@ -1,9 +1,13 @@
 import server from '.';
 
+jest.mock('webpack');
+jest.mock('webpack-dev-middleware');
+jest.mock('../webpack.config.js');
+
 jest.mock('express', () => require('jest-express'));
 
 describe('server', () => {
     it('listen http on 8080 port', () => {
-        expect(server.listen).toBeCalledWith(3000);
+        expect(server.listen).toBeCalledWith(8080, expect.any(Function));
     });
 });
