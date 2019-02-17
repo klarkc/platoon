69bb4bf9a282ccd8c9f5958bed076934ddf2aea0
Walker <walker@praiseweb.com.br>
2019-02-17 20:59:04 -0300

    fix scenes path

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
-import preload from 'preload';
-import create from 'create';
-import update from 'update';
+import preload from './preload';
+import create from './create';
+import update from './update';
 
 const config = {
     type: Phaser.AUTO,
