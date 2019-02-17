8a87abd0ee8f54b2f24f108d9fc40658c5448d71
Walker <walker@praiseweb.com.br>
2019-02-17 16:42:08 -0300

    add webpack support

--- index.js
+++
@@ -1,5 +0,0 @@
-const client = require('./client.js');
-
-client();
-
-console.log('Running on http://localhost:3000');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// const client = require('./client.js');
+
+// client();
+
+console.log('Running on http://localhost:3000');
