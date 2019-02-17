655518b5f65634dc67a6c5fd9cbb735d5bb4cad0
Walker <walker@praiseweb.com.br>
2019-02-17 16:31:43 -0300

    remove middleware

--- __tests__/client.spec.js
+++ __tests__/client.spec.js
@@ -4,15 +4,13 @@ const modulesMiddleware = require('modules-middleware');
 const client = require('../client.js');
 
 jest.mock('express', () => require('jest-express'));
-jest.mock('modules-middleware');
 
 describe('client', () => {
     it('set game middlewares', () => {
         const app = client();
         expect(express.static).toHaveBeenCalledWith('public');
-        expect(modulesMiddleware).toHaveBeenCalledWith('libs');
         expect(app).toBeInstanceOf(express.Express);
-        expect(app.use).toHaveBeenCalledTimes(2);
+        expect(app.use).toHaveBeenCalled();
     });
 
     it('listen on 3000 port in localhost', () => {
