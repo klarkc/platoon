07603de75f00d1ef8ae0bdf6d2d318d5351d88b7
Walker <walker@praiseweb.com.br>
2019-02-17 15:22:42 -0300

    add client tests

---
+++ client.spec.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const client = require('./client.js');
+
+jest.mock('express', () => require('jest-express'));
+
+describe('client', () => {
+    it('set game static directories', () => {
+        const app = client();
+        expect(express.static).toHaveBeenCalledWith('public');
+        expect(app).toBeInstanceOf(express.Express);
+        expect(app.use).toBeCalled();
+    });
+
+    it('listen on 3000 port', () => {
+        const app = client();
+        expect(app).toBeInstanceOf(express.Express);
+        expect(app.listen).toHaveBeenCalledWith(3000);
+    });
+})
